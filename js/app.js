92693b86135b772d74698e2408d97e06cec727e3
Stuart Black <stuartblack@gmail.com>
2018-09-07 12:45:57 +0100

    added form submit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
+
+  const newItemForm = document.querySelector('form#form-new-item');
+  newItemForm.addEventListener('submit', handleNewFormSubmit);
+
+
+
 })
+
+  const handleNewFormSubmit = function (formSubmitEvent){
+    event.preventDefault();
+
+    
+
+
+  }
