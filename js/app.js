5c9bea56c99b41cec7942e8ab1aea79092436faf
Stuart Black <stuartblack@gmail.com>
2018-09-08 11:46:12 +0100

    Added engine size logic

--- js/app.js
+++ js/app.js
@@ -7,9 +7,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const deleteAllButton = document.querySelector('#delete-all');
   deleteAllButton.addEventListener('click', handleDeleteAllClick);
 
+  const sortAllButton = document.querySelector('#sort-all');
+  sortAllButton.addEventListener('click', handleSortAllClick);
+
   console.log('JavaScript loaded form and delete');
 
-})
+});
 
 const handleNewItemFormSubmit = function (event){
   event.preventDefault();
@@ -17,37 +20,64 @@ const handleNewItemFormSubmit = function (event){
   const carList = document.querySelector('#car-list');
   carList.appendChild(carListItem);
   event.target.reset();
-}
+};
 
 const createCarListItem = function(form){
-  
-  console.dir(form)
-  const carListItem = document.createElement('li')
-  carListItem.classList.add('car-list-item')
-  console.log(carListItem);
+  const carListItem = document.createElement('li');
+  carListItem.classList.add('car-list-item');
 
   const manufacturer = document.createElement('h1');
   manufacturer.textContent = form.manufacturer.value;
-  console.log(manufacturer);
   carListItem.appendChild(manufacturer);
 
   const model = document.createElement('h2');
   model.textContent = form.model.value;
-  console.log(model);
   carListItem.appendChild(model);
 
   const category = document.createElement('h2');
   category.textContent = form.category.value;
-  console.dir(category);
   carListItem.appendChild(category);
 
+  const bhp = document.createElement('h2');
+  let randBhp = Math.floor((Math.random() * 60) + 950);
+
+  if (form.category.value == "LMP1" ){
+      bhp.textContent = `${randBhp} BHP - 1000 MAX`;
+      if (randBhp > 1000 ? bhp.textContent +=  ' **ILLEGAL**' : bhp.textContent +=  ' - PASSED' );
+
+  } else if (form.category.value == "LMP2") {
+      randBhp -= 200;
+      bhp.textContent = `${randBhp} BHP - 800 MAX`;
+      if (randBhp > 800 ? bhp.textContent +=  ' **ILLEGAL**' : bhp.textContent +=  ' - PASSED' );
+  } else if (form.category.value == "LMGTE_Pro" ) {
+      randBhp -= 400;
+      bhp.textContent = `${randBhp} BHP - 600 MAX`;
+      if (randBhp > 600 ? bhp.textContent +=  ' **ILLEGAL**' : bhp.textContent +=  ' - PASSED' );
+  } else if (form.category.value == "LMGTE_Am" ) {
+      randBhp -= 450;
+      bhp.textContent = `${randBhp} BHP - 550 MAX`;
+      if (randBhp > 600 ? bhp.textContent +=  ' **ILLEGAL**' : bhp.textContent +=  ' - PASSED' );
+  }
+
+    console.log(randBhp);
+  //TODO get all values of document bhp and average out SOF. Apply this to each cars BOP Change.
+  carListItem.appendChild(bhp);
+
   return carListItem;
-}
+};
+
+
+
+
+
 
 const handleDeleteAllClick = function (event) {
   const carList = document.querySelector('#car-list');
-  carList.innerHTML = '';
-}
+  carList.innerHTML = 'POOOOOOOOOO';
+};
 
- 
+const handleSortAllClick = function (event) {
+    const carList = document.querySelector('#car-list');
+    console.dir();
+};
 
